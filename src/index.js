8ffc097fc0ec8cc53e76c4cfd5e49e586c62dd43
choyoungkwang <choyoungkwang@nts-corp.com>
2022-04-25 14:28:08 +0900

    PURE REDUX: COUNTER

--- src/index.js
+++ src/index.js
@@ -4,9 +4,40 @@ const countBox = document.querySelector("#countBox");
 const addButton = document.querySelector("#add");
 const minusButton = document.querySelector("#minus");
 
-const countReducer = (count = 0) => {
-  console.log(count);
-  return count;
+let count = 0;
+const updateText = () => {
+  countBox.innerText = count;
 };
+
+/* Redux */
+const countReducer = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+
 const countStore = createStore(countReducer);
-console.log(countStore.getState());
+const onChange = () => {
+  count = countStore.getState();
+  updateText();
+};
+countStore.subscribe(onChange);
+
+/* Counter */
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+addButton.addEventListener("click", handleAdd);
+minusButton.addEventListener("click", handleMinus);
+
+/* Run */
+updateText();
