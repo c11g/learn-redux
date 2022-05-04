2be0fb74aea229cbb0c2c0281dada06ad367be7e
choyoungkwang <choyoungkwang@nts-corp.com>
2022-05-04 12:23:20 +0900

    Apply createAction

--- src/store.js
+++ src/store.js
@@ -1,40 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
 export const LOCAL_STORAGE_KEY = "todos_key";
 
 const loadState = localStorage.getItem(LOCAL_STORAGE_KEY);
 
 function reducer(state = loadState ? JSON.parse(loadState) : [], action) {
   switch (action.type) {
-    case ADD:
-      const newTodo = { text: action.payload.text, id: Date.now() };
+    case addAction.type:
+      const newTodo = { text: action.payload, id: Date.now() };
       return [newTodo, ...state];
-    case DELETE:
-      return state.filter((todo) => todo.id !== action.payload.id);
+    case deleteAction.type:
+      return state.filter((todo) => todo.id !== action.payload);
     default:
       return state;
   }
 }
 
-function addAction(text) {
-  return {
-    type: ADD,
-    payload: {
-      text,
-    },
-  };
-}
-
-function deleteAction(id) {
-  return {
-    type: DELETE,
-    payload: {
-      id,
-    },
-  };
-}
+const addAction = createAction("ADD");
+const deleteAction = createAction("DELETE");
 
 export const actionCreator = {
   addAction,
